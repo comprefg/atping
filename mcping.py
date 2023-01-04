c60f3a622e5ca44d531e112eeb4c6c404c51ac69
comprefg <39472523+comprefg@users.noreply.github.com>
2023-01-04 12:38:07 -0600

    Update mcping.py

--- mcping.py
+++ mcping.py
@@ -1,1 +1,14 @@
-a
+from mcstatus import JavaServer
+
+
+server = JavaServer.lookup("example.org:33711")
+
+
+status = server.status()
+print(f"The server has {status.players.online} player(s) online and replied in {status.latency} ms")
+
+latency = server.ping()
+print(f"The server replied in {latency} ms")
+
+query = server.query()
+print(f"The server has the following players online: {', '.join(query.players.names)}")
